5fcf88b511e33604a93c3530b2f39c958b68b328
mlinares <m.linares93@gmail.com>
2025-10-28 22:47:10 -0400

    create handleResources, need to fix

--- src/client.ts
+++ src/client.ts
@@ -1,5 +1,5 @@
 import "dotenv/config"
-import { confirm, input, select} from "@inquirer/prompts"
+import { confirm, input, select } from "@inquirer/prompts"
 import { Client } from "@modelcontextprotocol/sdk/client";
 import { StdioClientTransport } from "@modelcontextprotocol/sdk/client/stdio.js";
 import { Tool } from "@modelcontextprotocol/sdk/types.js"
@@ -44,22 +44,48 @@ async function main() {
           })),
         })
         const tool = tools.find(t => t.name === toolName)
-        if (tool == null){ 
+        if (tool == null) {
           console.error("Tool not found")
         }
         else {
           await handleTool(tool)
         }
-      break
+        break
+      case "Resources":
+        const resourceUri = await select({
+          message: "Select a resource",
+          choices: [
+            ...resources.map(resource => ({
+              name: resource.name,
+              value: resource.uri,
+              description: resource.description,
+            })),
+            ...resourceTemplates.map(template => ({
+              name: template.name,
+              value: template.uriTemplate,
+              description: template.description,
+            })),
+          ],
+        })
+        const uri =
+          resources.find(r => r.uri === resourceUri)?.uri ??
+          resourceTemplates.find(r => r.uriTemplate === resourceUri)
+            ?.uriTemplate
+        if (uri == null) {
+          console.error("Resource not found.")
+        } else {
+          await handleResource(uri)
+        }
+        break
     }
   }
 }
 
-async function handleTool(tool: Tool){
-  const args: Record<string,string> = {}
-  for(const [key, value] of Object.entries(tool.inputSchema.properties ?? {})){
+async function handleTool(tool: Tool) {
+  const args: Record<string, string> = {}
+  for (const [key, value] of Object.entries(tool.inputSchema.properties ?? {})) {
     args[key] = await input({
-      message: `Enter valve for ${key} (${(value as {type: string}).type}):`,
+      message: `Enter valve for ${key} (${(value as { type: string }).type}):`,
     })
   }
   const res = await mcp.callTool({
@@ -68,6 +94,29 @@ async function handleTool(tool: Tool){
   })
 
   console.log((res.content as [{ text: string }])[0].text)
+}
+
+async function handleResource(uri: string) {
+  let finalUri = uri
+  const paramMatches = uri.match(/{([^}]+)}/g)
+
+  if (paramMatches != null) {
+    for (const paramMatch of paramMatches) {
+      const paramName = paramMatch.replace("{", "").replace("}", "")
+      const paramValue = await input({
+        message: `Enter value for ${paramName}:`,
+      })
+      finalUri = finalUri.replace(paramMatch, paramValue)
+    }
+  }
+
+  const res = await mcp.readResource({
+    uri: finalUri,
+  })
+
+  console.log(
+    JSON.stringify(JSON.parse(res.contents[0].text as string), null, 2)
+  )
 }
 
 main()
